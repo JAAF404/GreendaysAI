cb17eb8a75dac519c02d033837e5012bf36f0e1c
JAAF <100472568+JAAF404@users.noreply.github.com>
2022-05-26 08:54:39 +0200

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -28,29 +28,29 @@ def main():
 
     #escoger el modelo preferido
 
-# 1. as sidebar menu
-with st.sidebar:
-    selected = option_menu("Main Menu", ["Home", 'Settings'], 
-        icons=['house', 'gear'], menu_icon="cast", default_index=1)
-    selected
+    # 1. as sidebar menu
+    with st.sidebar:
+        selected = option_menu("Main Menu", ["Home", 'Settings'], 
+            icons=['house', 'gear'], menu_icon="cast", default_index=1)
+        selected
 
-# 2. horizontal menu
-selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    menu_icon="cast", default_index=0, orientation="horizontal")
-selected2
+    # 2. horizontal menu
+    selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
+        icons=['house', 'cloud-upload', "list-task", 'gear'], 
+        menu_icon="cast", default_index=0, orientation="horizontal")
+    selected2
 
-# 3. CSS style definitions
-selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
-    icons=['house', 'cloud-upload', "list-task", 'gear'], 
-    menu_icon="cast", default_index=0, orientation="horizontal",
-    styles={
-        "container": {"padding": "0!important", "background-color": "#fafafa"},
-        "icon": {"color": "orange", "font-size": "25px"}, 
-        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"background-color": "green"},
-    }
-)
+    # 3. CSS style definitions
+    selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
+        icons=['house', 'cloud-upload', "list-task", 'gear'], 
+        menu_icon="cast", default_index=0, orientation="horizontal",
+        styles={
+            "container": {"padding": "0!important", "background-color": "#fafafa"},
+            "icon": {"color": "orange", "font-size": "25px"}, 
+            "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+            "nav-link-selected": {"background-color": "green"},
+        }
+    )
     
 #    option = ['si', 'también', 'a que va a ser que si...']
 #    model = st.sidebar.selectbox('tu eres tonto?', option)
