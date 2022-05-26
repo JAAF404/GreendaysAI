2da9336cb4a3bf612cbea6045a8d5241d907d752
JAAF <100472568+JAAF404@users.noreply.github.com>
2022-05-26 15:59:28 +0200

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -1,7 +1,6 @@
 #importar librerias
 import streamlit as st
-#rom streamlit_option_menu import option_menu
-#import pickle
+import pickle
 import pandas as pd
 from PIL import Image
 def main():
@@ -9,13 +8,30 @@ def main():
     st.title('prueba')
     #titulo de sidebar
     st.sidebar.header('Menu')
-    
+
+    #funcion para poner los parametrso en el sidebar
+    def user_input_parameters():
+        a = st.sidebar.slider('aux1', 0.0, 0.5, 1.0)
+        b = st.sidebar.slider('aux2', 0.0, 0.5, 1.0)
+        c = st.sidebar.slider('aux3', 0.0, 0.5, 1.0)
+        d = st.sidebar.slider('aux4', 0.0, 0.5, 1.0)
+        data = {'a': a,
+                'b': b,
+                'c': c,
+                'd': d,
+                }
+        features = pd.DataFrame(data, index=[0])
+        return features
+
+    df = user_input_parameters()
+
+    #escoger el modelo preferido
     option = ['si', 'también', 'a que va a ser que si...']
     model = st.sidebar.selectbox('tu eres tonto?', option)
 
-    st.subheader('User Input Parameters')
-    st.subheader(model)
-    st.write(df)
+    # st.subheader('User Input Parameters')
+    # st.subheader(model)
+    # st.write(df)
 
     # if st.button('RUN'):
     #     if model == 'modelo a':
@@ -32,4 +48,3 @@ def main():
       
 if __name__ == '__main__':
     main()
-    
