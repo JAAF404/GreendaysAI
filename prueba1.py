5d596ff4df92b8e169e964e6cf2891e496958f43
JAAF <100472568+JAAF404@users.noreply.github.com>
2022-05-26 08:49:30 +0200

    Update prueba1.py

--- prueba1.py
+++ prueba1.py
@@ -1,33 +1,59 @@
 #importar librerias
 import streamlit as st
-import pickle
+from streamlit_option_menu import option_menu
+#import pickle
 import pandas as pd
 from PIL import Image
 def main():
     #titulo
     st.title('prueba')
     #titulo de sidebar
-    st.sidebar.header('Menu')
+#    st.sidebar.header('Menu')
 
     #funcion para poner los parametrso en el sidebar
-    def user_input_parameters():
-        a = st.sidebar.slider('aux1', 0.0, 0.5, 1.0)
-        b = st.sidebar.slider('aux2', 0.0, 0.5, 1.0)
-        c = st.sidebar.slider('aux3', 0.0, 0.5, 1.0)
-        d = st.sidebar.slider('aux4', 0.0, 0.5, 1.0)
-        data = {'a': a,
-                'b': b,
-                'c': c,
-                'd': d,
-                }
-        features = pd.DataFrame(data, index=[0])
-        return features
-
-    df = user_input_parameters()
+#    def user_input_parameters():
+#        a = st.sidebar.slider('aux1', 0.0, 0.5, 1.0)
+#        b = st.sidebar.slider('aux2', 0.0, 0.5, 1.0)
+#        c = st.sidebar.slider('aux3', 0.0, 0.5, 1.0)
+#        d = st.sidebar.slider('aux4', 0.0, 0.5, 1.0)
+#        data = {'a': a,
+#                'b': b,
+#                'c': c,
+#                'd': d,
+#                }
+#        features = pd.DataFrame(data, index=[0])
+#        return features
+#
+#    df = user_input_parameters()
 
     #escoger el modelo preferido
-    option = ['si', 'también', 'a que va a ser que si...']
-    model = st.sidebar.selectbox('tu eres tonto?', option)
+
+# 1. as sidebar menu
+with st.sidebar:
+    selected = option_menu("Main Menu", ["Home", 'Settings'], 
+        icons=['house', 'gear'], menu_icon="cast", default_index=1)
+    selected
+
+# 2. horizontal menu
+selected2 = option_menu(None, ["Home", "Upload", "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal")
+selected2
+
+# 3. CSS style definitions
+selected3 = option_menu(None, ["Home", "Upload",  "Tasks", 'Settings'], 
+    icons=['house', 'cloud-upload', "list-task", 'gear'], 
+    menu_icon="cast", default_index=0, orientation="horizontal",
+    styles={
+        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "icon": {"color": "orange", "font-size": "25px"}, 
+        "nav-link": {"font-size": "25px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "green"},
+    }
+)
+    
+#    option = ['si', 'también', 'a que va a ser que si...']
+#    model = st.sidebar.selectbox('tu eres tonto?', option)
 
     # st.subheader('User Input Parameters')
     # st.subheader(model)
